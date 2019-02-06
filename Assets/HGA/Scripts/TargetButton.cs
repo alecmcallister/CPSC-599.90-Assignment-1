e185e6365e5fc8173e17bfaf2114ac97b2ecdaee
Alec McAllister <alec@hazardousgiraffe.com>
2019-02-05 19:20:29 -0700

    Here's all the things.

---
+++ Assets/HGA/Scripts/TargetButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class TargetButton : MonoBehaviour, IPointerClickHandler
+{
+	public static event Action<GameObject, bool> TargetClicked;
+	public bool finish;
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		TargetClicked?.Invoke(transform.root.gameObject, finish);
+	}
+}
